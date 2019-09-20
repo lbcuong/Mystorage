43f11b14b4316b39c143599fb65f4667371be700
Le Cuong <lecuong1521999@gmail.com>
2019-09-20 15:43:28 +0700

    commit

--- PythonTest/PythonTest.py
+++ PythonTest/PythonTest.py
@@ -1,16 +1,50 @@
-def isPalindrome(str): 
+import random
+
+def rockPaperScissor():
+    computer = ['rock', 'paper', 'scissor']
+    while True:
+        user = input("Your selection is: ")
+        computerSelection= random.choice(computer)
+        print("Computer selection :", computerSelection)
+        if ((user == 'rock') or (user == 'paper') or (user == 'scissor')):
+            if user == 'rock':
+                if computerSelection == 'paper':
+                    print("You lose!!!")
+                elif computerSelection == 'scissor':
+                    print("You win!!!")
+                else:
+                    print("Drawwwww")
+            if user == 'paper':
+                if computerSelection == 'scissor':
+                    print("You lose!!!")
+                elif computerSelection == 'rock':
+                    print("You win!!!")
+                else:
+                    print("Drawwwww")
+            if user == 'scissor':
+                if computerSelection == 'rock':
+                    print("You lose!!!")
+                elif computerSelection == 'paper':
+                    print("You win!!!")
+                else:
+                    print("Drawwwww")
+            break
+        else:
+            print("Incorrect")
+
+rockPaperScissor()
+
+while True:
+    print("Do you want to play a new game? Y|N")
+    answer = input()
+    if answer == 'Y':
+        print("Start a new game!")
+        rockPaperScissor()
+    elif answer == 'N':
+        break
+    else:
+        print("Please type Y or N")
+            
+        
+
   
-    # Run loop from 0 to len/2
-    for i in range(0, (len(str))):
-        if str[i] != str[len(str)-i-1]: 
-            return False
-    return True
-  
-# main function 
-s = "malayalam"
-ans = isPalindrome(s) 
-  
-if (ans): 
-    print("Yes") 
-else: 
-    print("No") 
